1f2d79002fd166d3e05f7292ac208ae9604f1625
Charles Bergeron <chuck.bergeron@gmail.com>
2018-12-05 10:44:26 -0800

    * fixing layout stuff

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,21 +7,19 @@ class Nav extends React.Component {
   render() {
     return (
       <div className="container">
-        <div className="twelve columns">
-          <div className="nav-grid">
-            <Link className="brand" to="/" title="Delta Camp Logo">
-              <LogoSvg />
-            </Link>
+        <div className="nav-grid">
+          <Link className="brand" to="/" title="Delta Camp Logo">
+            <LogoSvg />
+          </Link>
 
-            <ul className="nav work-layout--nav">
-              <li className="nav--list-item">
-                <Link className="nav--link" to="/work" title="See Our Work">Work</Link>
-              </li>
-              <li className="nav--list-item">
-                <Link className="nav--link" to="/contact" title="Contact Us">Contact</Link>
-              </li>
-            </ul>
-          </div>
+          <ul className="nav work-layout--nav">
+            <li className="nav--list-item">
+              <Link className="nav--link" to="/work" title="See Our Work">Work</Link>
+            </li>
+            <li className="nav--list-item">
+              <Link className="nav--link" to="/contact" title="Contact Us">Contact</Link>
+            </li>
+          </ul>
         </div>
       </div>
     )
