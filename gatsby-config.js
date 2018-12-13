4bbf4583ae975b063b364d6d40c014c28962182d
Brendan Asselstine <mail.asselstine@gmail.com>
2018-12-13 13:03:06 -0800

    Added Google Analytics trackingId

--- gatsby-config.js
+++ gatsby-config.js
@@ -94,7 +94,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-131029656-1`
       },
     },
     `gatsby-plugin-feed`,
